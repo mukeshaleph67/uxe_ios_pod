8d0545b6cbcce6c099fdf6b41449a151069c0a63
Mukesh Varma <mukesh@aleph-labs.com>
2019-03-04 12:00:25 +0800

    first commint

---
+++ Classes/UIColor+DesignTokens/UIColor+DesignTokens.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UIColor {
+    static let brand_background_black1 = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    static let brand_background_black2 = #colorLiteral(red: 0.2166400552, green: 0.2166830003, blue: 0.2166343629, alpha: 1)
+    static let brand_background_black3 = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+    static let brand_background_dark1 = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
+    static let brand_background_dark2 = #colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+    static let brand_background_dark3 = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+    static let brand_background_light1 = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+    static let brand_background_light2 = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    static let brand_background_light3 = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    static let brand_background_base1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+    static let brand_background_base2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    static let brand_background_base3 = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+}
